120ec74171f24b5993ae2328ddd3849799bea8bd
pshnghng0318 <pshnghng@gmail.com>
2026-02-23 10:20:14 +0800

    Update FileBrowserStore and protobuf enums

--- src/stores/FileBrowserStore/FileBrowserStore.ts
+++ src/stores/FileBrowserStore/FileBrowserStore.ts
@@ -32,7 +32,7 @@ export enum SelectionMode {
 }
 
 export type RegionFileType = CARTA.FileType.CRTF | CARTA.FileType.DS9_REG;
-export type ImageFileType = CARTA.FileType.CASA | CARTA.FileType.FITS | CARTA.FileType.HDF5 | CARTA.FileType.MIRIAD;
+export type ImageFileType = CARTA.FileType.CASA | CARTA.FileType.FITS | CARTA.FileType.HDF5 | CARTA.FileType.MIRIAD | CARTA.FileType.ZARR;
 export type CatalogFileType = CARTA.CatalogFileType.VOTable | CARTA.CatalogFileType.FITSTable;
 
 type DirectoryInfo = CARTA.IFileInfo & Omit<CARTA.IDirectoryInfo, keyof CARTA.IFileInfo>;
@@ -112,7 +112,7 @@ export class FileBrowserStore {
             if (activeFrame) {
                 // Update channelValueBounds for save image
                 FileBrowserStore.Instance.initialSaveSpectralRange();
-                this.setSaveFileType(activeFrame.frameInfo?.fileInfo.type === CARTA.FileType.CASA ? CARTA.FileType.CASA : CARTA.FileType.FITS);
+                this.setSaveFileType(activeFrame.frameInfo?.fileInfo.type === CARTA.FileType.CASA ? CARTA.FileType.CASA : activeFrame.frameInfo?.fileInfo.type === CARTA.FileType.ZARR ? CARTA.FileType.ZARR : CARTA.FileType.FITS);
 
                 // update regions
                 this.resetExportRegionIndexes(SelectionMode.All);
